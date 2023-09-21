07943aa270fa650332e2ca15a57d89518e38849d
Clifton Labrum <clifton@labrum.co>
2023-09-21 12:56:55 -0700

    Stuff.

---
+++ src/lib/AppCard.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	export let name: string;
+	export let title: string;
+	export let details: string;
+	export let url: string;
+	export let link: string;
+	export let platforms: string;
+</script>
+
+<div class="app">
+	<a href={url} class={name} target="_blank">
+		{#if name == 'avid'}
+			<img src="/images/avid.png" width="90" height="90" alt="Avid.pro" />
+		{:else if name == 'pilotpro'}
+			<img src="/images/pilotpro.png" width="90" height="90" alt="PilotPro.com" />
+		{:else}
+			<img src="/images/wardhub.png" width="90" height="90" alt="WardHub.org" />
+		{/if}
+
+		<h2>{title}</h2>
+		<p>{details}</p>
+		<h3>{link}</h3>
+		<small>{platforms}</small>
+	</a>
+</div>
+
+<style>
+	.app {
+		text-align: center;
+		margin: 20px;
+	}
+	/* a.pilotpro h2 {
+		margin-top: 4px;
+	} */
+	a {
+		display: block;
+		font-size: 12px;
+		text-decoration: none;
+		background: rgba(0, 0, 0, 0.2);
+		border-radius: 10px;
+		padding: 20px 0px;
+		width: 250px;
+		transition: background-color 0.3s ease;
+	}
+	a:hover {
+		background: rgba(0, 0, 0, 0.3);
+	}
+
+	h2 {
+		color: #fff;
+		font-size: 18px;
+		margin: 0;
+		font-weight: normal;
+	}
+	h3 {
+		color: #3fa3f5;
+		font-weight: normal;
+		font-size: 14px;
+		margin: 0;
+	}
+	p {
+		font-size: 14px;
+		color: #bebfc2;
+		margin: 10px 15px;
+		line-height: 1.3;
+	}
+	small {
+		display: block;
+		font-size: 10px;
+		color: #777879;
+		margin: 15px auto 5px;
+		font-weight: 500;
+	}
+
+	@media screen and (max-width: 568px) {
+		.app {
+			margin: 20px auto;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+	import '$lib/fonts.css';
+	import '$lib/global.css';
+	import Icons from '$lib/Icons.svelte';
+	import AppCard from '$lib/AppCard.svelte';
+	let year = new Date().getFullYear();
+</script>
+
+<svelte:head>
+	<title>Labrum Co • App Design &amp; Development</title>
+</svelte:head>
+
+<div id="app">
+	<h1 title="Labrum Co. LC">
+		<Icons name="logo" width="191" height="56" />
+	</h1>
+	<h3 title="App Design & Development">
+		<Icons name="appDesign" width="284" height="31" />
+	</h3>
+
+	<div class="apps">
+		<AppCard
+			name="avid"
+			title="Avid"
+			details="Notes, tasks, projects, workflows, subtasks, & more."
+			url="https://avid.pro"
+			link="avid.pro"
+			platforms="Mac • iOS • Android • Windows"
+		/>
+
+		<AppCard
+			name="pilotpro"
+			title="Pilot Pro"
+			details="A beautiful and powerful logbook for pilots."
+			url="https://pilotpro.com"
+			link="pilotpro.com"
+			platforms="Mac • iOS • Android • Windows"
+		/>
+	</div>
+
+	<div id="footer">
+		<p>Copyright &copy; {year} &nbsp; Labrum Co.</p>
+	</div>
+</div>
+
+<style>
+	#app {
+		text-align: center;
+	}
+	h1 {
+		color: #fff;
+		margin: 60px auto 0px;
+		font-weight: normal;
+		text-align: center;
+	}
+	h3 {
+		margin: 0;
+	}
+
+	/* --- Apps --- */
+	.apps {
+		display: flex;
+		margin: 0 auto;
+		max-width: 600px;
+		justify-content: center;
+	}
+
+	/* --- Footer --- */
+	#footer {
+		padding-bottom: 30px;
+	}
+	#footer p {
+		font-size: 10px;
+		color: #5f6061;
+	}
+	/* --- Responsive --- */
+	@media screen and (max-width: 568px) {
+		.apps {
+			flex-direction: column;
+			text-align: center;
+		}
+	}
+</style>
